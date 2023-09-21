39665415ede1428a9a4e8434452a81c19dd98f28
Anna Moraco <annamoraco@gmail.com>
2023-09-21 20:44:41 -0300

    reverstring function ready

--- reversestring.cpp
+++ reversestring.cpp
@@ -4,15 +4,30 @@
 
 using namespace std;
 
-string reversestring(string string){
-    return string;
+string reversestring(string input_str){
+
+    int str_len = input_str.length();
+
+    int str_position = 1;
+
+    while(str_position <= str_len){
+        
+        input_str = input_str[str_position] + input_str;
+
+        input_str.erase(str_position + 1, 1);
+
+        str_position++;
+
+    }
+
+    return input_str;
 }
 
 int main()
 {
-    string test_string = "string";
-
-    int str_len = test_string.length();
+    string test_string;
+    cout << endl << "Type a string: ";
+    cin >> test_string;
 
     cout << reversestring(test_string);
 }
