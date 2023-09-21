f91d957bdff3c91a0df4c16f69dfcd82c26d3657
Anna Moraco <annamoraco@gmail.com>
2023-09-21 20:37:55 -0300

    Main structure

---
+++ reversestring.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<string>
+#include<cstring>
+
+using namespace std;
+
+string reversestring(string string){
+    return string;
+}
+
+int main()
+{
+    string test_string = "string";
+
+    int str_len = test_string.length();
+
+    cout << reversestring(test_string);
+}
